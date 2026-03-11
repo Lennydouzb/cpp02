c240cfb967687ffb3ee77a1086897196bf4c4948
Lenny Desbouis <ldesboui@2D1.42angouleme.fr>
2026-03-11 14:50:02 +0100

    Commit

--- ex03/bsp.cpp
+++ ex03/bsp.cpp
@@ -6,7 +6,7 @@
 /*   By: ldesboui <ldesboui@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/10 12:38:34 by ldesboui          #+#    #+#             */
-/*   Updated: 2026/03/10 19:40:55 by ldesboui         ###   ########.fr       */
+/*   Updated: 2026/03/11 14:45:16 by ldesboui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Point.hpp"
@@ -14,15 +14,15 @@
 bool	bsp( Point const a, Point const b, Point const c, Point const point)
 {
 	Fixed abp = ((b.getX() - a.getX()) * (point.getY() - a.getY()))
-		- ((b.getY() - a.getY()) * (p.getX() - a.getX());
-	Fixed acp = (c.getX() - a.getX()) * (point.getY() - a.getY());
-		- ((b.getY() - a.getY()) * (p.getX() - a.getX());
-	Fixed bcp = (c.getX() - b.getX()) * (point.getY() - b.getY());
-		- ((b.getY() - a.getY()) * (p.getX() - a.getX());
+		- ((b.getY() - a.getY()) * (point.getX() - a.getX()));
+	Fixed bcp = ((c.getX() - b.getX()) * (point.getY() - b.getY()))
+		- ((c.getY() - b.getY()) * (point.getX() - b.getX()));
+	Fixed cap = ((a.getX() - c.getX()) * (point.getY() - c.getY()))
+		- ((a.getY() - c.getY()) * (point.getX() - c.getX()));
 
-	bool has_zero = abp == 0 || acp == 0 || bcp == 0;
-	bool has_neg = abp < 0 || acp < 0 || bcp < 0;
-	bool has_pos = abp > 0 || acp > 0 || bcp > 0;
+	bool has_zero = abp == 0 || cap == 0 || bcp == 0;
+	bool has_neg = abp < 0 || cap < 0 || bcp < 0;
+	bool has_pos = abp > 0 || cap > 0 || bcp > 0;
 	
 	// on a line
 	if (has_zero)
--- ex03/main.cpp
+++ ex03/main.cpp
@@ -6,7 +6,7 @@
 /*   By: ldesboui <ldesboui@42angouleme.fr>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/03/06 14:23:58 by ldesboui          #+#    #+#             */
-/*   Updated: 2026/03/10 18:47:55 by ldesboui         ###   ########.fr       */
+/*   Updated: 2026/03/11 14:43:51 by ldesboui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include "Point.hpp"
@@ -14,11 +14,18 @@
 
 int main( void )
 {
-	const Point A(0, 0);
-	const Point B(1, 0);
-	const Point C(0.5f, 1);
+	const Point A(0.0f, 0.0f);
+	const Point B(1.0f, 0.0f);
+	const Point C(0.5f, 1.0f);
 
 	const Point P(0.2f, 0.3f);
+	const Point Pp(0.2f, 1.1f);
+	const Point Ppp(0.73137f, 0.77247f);
+	const Point Pppp(0.76118f, 0.3423f);
 
-	std::cout << bsp(A, B, C, P) <<  std::endl; 
+	std::cout << "is in :" <<  bsp(A, B, C, P) <<  std::endl; 
+	std::cout << "is in :" <<  bsp(A, B, C, Pppp) <<  std::endl; 
+
+	std::cout << "is not in :" <<  bsp(A, B, C, Pp) <<  std::endl; 
+	std::cout << "is not in :" <<  bsp(A, B, C, Ppp) <<  std::endl; 
 }	
